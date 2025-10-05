f36657eb1aa82e52d9046523535635bd89b1ab92
zzeyzeyz16-dev <zzeyzeyz16@gmail.com>
2025-10-05 15:38:10 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,8 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 local Sprinting = game:GetService("ReplicatedStorage").Systems.Character.Game.Sprinting
 local stamina = require(Sprinting)
-energy.MaxEnergy = 230  -- Maximum stamina
-energy.MinEnergy = -20  -- Minimum stamina
-energy.EnergyGain = 100 -- Stamina gain
-energy.EnergyLoss = 5 -- Stamina loss
-energy.SprintSpeed = 40 -- Sprint speed
-energy.EnergyLossDisabled = true -- Disable stamina drain (true/false)
+energy.MaxEnergy = 230  
+energy.MinEnergy = -20  
+energy.EnergyGain = 100 
+energy.EnergyLoss = 5 
+energy.SprintSpeed = 40 
+energy.EnergyLossDisabled = true 
